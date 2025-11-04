bbf519162d7646ca8194d6e8c741e4cc23b65fb9
wildanfm19 <dracko.maaruftm@gmail.com>
2025-11-04 17:19:41 +0700

    fix : nba logo not appear when deploy

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import nbaLogo from '../assets/images/nba-logo.png'
 
 const Logo = () => {
   return (
     <div className='logo w-15 h-15'>
-        <img src='/src/assets/images/nba-logo.png' alt='NBA logo' className='w-full h-full object-contain'/>
+        <img src={nbaLogo} alt='NBA logo' className='w-full h-full object-contain'/>
     </div>
   )
 }
